fa83675019fa0e2fa341503e664d40a380b6c500
JeffRisberg <j.s.risberg@gmail.com>
2023-04-09 23:46:03 -0700

    start the Donation repository

--- src/main/java/com/company/DonationRepository.java
+++ src/main/java/com/company/DonationRepository.java
@@ -1,27 +1,23 @@
 package com.company;
 
 import com.company.domain.Donation;
-import io.micronaut.core.annotation.NonNull;
-import io.micronaut.data.annotation.Id;
 import io.micronaut.data.exceptions.DataAccessException;
 import io.micronaut.data.jdbc.annotation.JdbcRepository;
 import io.micronaut.data.model.query.builder.sql.Dialect;
 import io.micronaut.data.repository.PageableRepository;
 
 import javax.transaction.Transactional;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 
 @JdbcRepository(dialect = Dialect.MYSQL)
 public interface DonationRepository extends PageableRepository<Donation, Long> {
 
-  Donation save(@NonNull @NotBlank String firstName, @NonNull @NotBlank String lastName, String address);
+  Donation save();
 
   @Transactional
-  default Donation saveWithException(@NonNull @NotBlank String firstName, @NonNull @NotBlank String lastName, String address) {
-    save(firstName, lastName, address);
+  default Donation saveWithException() {
+    save();
     throw new DataAccessException("test exception");
   }
 
-  long update(@NonNull @NotNull @Id Long id, @NonNull @NotBlank String firstName, @NotNull @NotBlank String lastName, String address);
+  long update();
 }
